1a12fce0640ca7b87e33637516d64b9683a06223
MPapiol <107648933+MPapiol@users.noreply.github.com>
2023-10-26 16:48:19 +0200

    Update GestionOS.java
    No tengo claro que vaya por buen camino

--- DataCrafters/DataCrafters/src/datacrafters/vista/GestionOS.java
+++ DataCrafters/DataCrafters/src/datacrafters/vista/GestionOS.java
@@ -14,23 +14,23 @@ public class GestionOS {
 
     public void inicio() {
         boolean salir = false;
-        char opcio;
+        char opcion;
 
         do {
             System.out.println("1. Gestión Articulos");
             System.out.println("2. Gestión Clientes");
             System.out.println("3. Gestión Pedidos");
             System.out.println("0. Salir");
-            opcio = pedirOpcion();
-            switch (opcio) {
+            opcion = pedirOpcion();
+            switch (opcion) {
                 case '1':
-                    // TO-BE-DONE
+                    gestionArticulos();
                     break;
                 case '2':
-                    // TO-BE-DONE
+                    gestionClientes();
                     break;
                 case '3':
-                    // TO-BE-DONE
+                    gestionPedidos();
                     break;
                 case '0':
                     salir = true;
@@ -47,4 +47,149 @@ public class GestionOS {
         }
         return resp.charAt(0);
     }
+
+    void gestionArticulos() {
+        boolean salir = false;
+        char opcion;
+
+        do {
+            System.out.println("1. Añadir Articulos");
+            System.out.println("2. Mostrar Articulos");
+            System.out.println("0. Salir");
+            opcion = pedirOpcionGestionArticulos();
+            switch (opcion) {
+                case '1':
+                    añadirArticulo();
+                    break;
+                case '2':
+                    mostrarArticulo();
+                    break;
+                case '0':
+                    salir = true;
+            }
+        } while (!salir);
+    }
+
+    char pedirOpcionGestionArticulos() {
+        String resp;
+        System.out.println("Elige una opción (1,2 o 0):");
+        resp = teclado.nextLine();
+        if (resp.isEmpty()) {
+            resp = " ";
+        }
+        return resp.charAt(0);
+    }
+
+    void añadirArticulo() {
+    }
+
+    void mostrarArticulo() {
+    }
+
+    void gestionClientes() {
+        boolean salir = false;
+        char opcion;
+
+        do {
+            System.out.println("1. Añadir Clientes");
+            System.out.println("2. Mostrar Clientes");
+            System.out.println("3. Mostrar Clientes Estandar");
+            System.out.println("4. Mostrar Clientes Premium");
+            System.out.println("0. Salir");
+            opcion = pedirOpcionGestionArticulos();
+            switch (opcion) {
+                case '1':
+                    añadirCliente();
+                    break;
+                case '2':
+                    mostrarCliente();
+                    break;
+                case '3':
+                    mostrarClienteEstandard();
+                    break;
+                case '4':
+                    mostrarClientePremium();
+                    break;
+                case '0':
+                    salir = true;
+            }
+        } while (!salir);
+    }
+
+    char pedirOpcionGestionClientes() {
+        String resp;
+        System.out.println("Elige una opción (1,2 o 0):");
+        resp = teclado.nextLine();
+        if (resp.isEmpty()) {
+            resp = " ";
+        }
+        return resp.charAt(0);
+    }
+
+    void añadirCliente() {
+    }
+
+    void mostrarCliente() {
+    }
+
+    void mostrarClienteEstandard() {
+
+    }
+
+    void mostrarClientePremium() {
+
+    }
+
+    void gestionPedidos() {
+        boolean salir = false;
+        char opcion;
+
+        do {
+            System.out.println("1. Añadir Pedido");
+            System.out.println("2. Eliminar Pedido");
+            System.out.println("3. Mostrar Pedido Enviado");
+            System.out.println("4. Mostrar Pedido En Curso");
+            System.out.println("0. Salir");
+            opcion = pedirOpcionGestionPedidos();
+            switch (opcion) {
+                case '1':
+                    añadirPedido();
+                    break;
+                case '2':
+                    eliminarPedido();
+                    break;
+                case '3':
+                    mostrarPedidoEnviado();
+                    break;
+                case '4':
+                    mostrarPedidoEnCurso();
+                    break;
+                case '0':
+                    salir = true;
+            }
+        } while (!salir);
+    }
+
+    char pedirOpcionGestionPedidos() {
+        String resp;
+        System.out.println("Elige una opción (1,2 o 0):");
+        resp = teclado.nextLine();
+        if (resp.isEmpty()) {
+            resp = " ";
+        }
+        return resp.charAt(0);
+    }
+
+    void añadirPedido() {
+    }
+
+    void eliminarPedido() {
+    }
+
+    void mostrarPedidoEnviado() {
+    }
+
+    void mostrarPedidoEnCurso() {
+    }
 }
+
