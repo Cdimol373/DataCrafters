77583652fc94c23a8c46bca9710787ca9504c8a6
Josep <josep@tecnologiesindustrials.com>
2023-10-30 11:22:20 +0100

    string añadido

--- Producto2/src/Modelo/Pedidos.java
+++ Producto2/src/Modelo/Pedidos.java
@@ -5,17 +5,19 @@ import java.time.LocalDateTime;
 public class Pedidos {
     private int numeroPedido;
     private Cliente cliente;
-    private Articulos articulo;  // Asegúrate de que el nombre de la clase sea 'Articulos' como en tu descripción
+    private Articulos articulo;
     private int cantidad;
     private LocalDateTime fechaHora;
+    private boolean enviado;
 
     // Constructor
-    public Pedidos(int numeroPedido, Cliente cliente, Articulos articulo, int cantidad, LocalDateTime fechaHora) {
+    public Pedidos(int numeroPedido, Cliente cliente, Articulos articulo, int cantidad, LocalDateTime fechaHora, boolean enviado) {
         this.numeroPedido = numeroPedido;
         this.cliente = cliente;
         this.articulo = articulo;
         this.cantidad = cantidad;
         this.fechaHora = fechaHora;
+        this.enviado = false;
     }
 
     // Getters y Setters
@@ -68,14 +70,44 @@ public class Pedidos {
         return (precioArticulo * cantidad) + descuentoEnvio;
     }
 
-    @Override
-    public String toString() {
+    // Método para marcar el pedido como enviado
+    public boolean pedidoEnviado() {
+        this.enviado = true;
+        return this.enviado;
+    }
+
+    // Método para obtener el estado de envío del pedido
+    public boolean comprobarEnviado() {
+        return this.enviado;
+    }
+
+    /*public String toString() {
         return "Pedidos{" +
                 "numeroPedido=" + numeroPedido +
                 ", cliente=" + cliente +
                 ", articulo=" + articulo +
                 ", cantidad=" + cantidad +
                 ", fechaHora=" + fechaHora +
+                '}';*/
+
+
+    @Override
+    public String toString() {
+        double precioTotal = calcularPrecio();
+        double costeEnvio = articulo.getGastosEnvio();
+        return "Pedidos{" +
+                "numeroPedido=" + numeroPedido +
+                ", fechaHora=" + fechaHora +
+                ", Nif del cliente=" + cliente.getNif() +  // Asumiendo que tienes un getter para NIF en la clase Cliente
+                ", nombre del cliente=" + cliente.getNombre() +  // Asumiendo que tienes un getter para nombre en la clase Cliente
+                ", codigo del articulo=" + articulo.getCodigo() +  // Asumiendo que tienes un getter para código en la clase Articulos
+                ", descripcion del articulo=" + articulo.getDescripcion() +  // Asumiendo que tienes un getter para descripción en la clase Articulos
+                ", cantidad=" + cantidad +
+                ", precio del articulo=" + articulo.getPrecio() +  // Asumiendo que tienes un getter para precio en la clase Articulos
+                ", coste de envio=" + costeEnvio +
+                ", precio total=" + precioTotal +
+                ", enviado=" + (enviado ? "Si" : "No") +  // Mostrará "Si" si enviado es true, "No" en caso contrario
                 '}';
     }
+
 }
